ad0b3eb80c0c9f4dc7f406c902bb6b49b94b6366
Rachele-Turtora <racheleturtora21@gmail.com>
2024-05-07 14:34:00 +0200

    [UPD] checks

--- palindroma/js/main.js
+++ palindroma/js/main.js
@@ -5,13 +5,26 @@ const word = prompt("Inserisci una parola");
 
 // Checking if the word is a palindrome
 function palindroma(myWord){
-    const reverseWord = myWord.split('').reverse().join(''); // trasformo la stringa in array, lo inverto e lo ritrasformo in stringa
-    if (reverseWord == myWord){
-        return `${myWord} è palindroma`;
+
+    // Checking if the string is a phrase and converting to lowercase
+    let wordToCheck;
+    if (myWord.length > 1){
+        wordToCheck = myWord.split(" ").join("").toLowerCase();
     } else {
-        return `${myWord} non è palindroma`;
+        wordToCheck = myWord.toLowerCase();
+    }
+
+    const reverseWord = wordToCheck.split('').reverse().join(''); // trasformo la stringa in array, lo inverto e lo ritrasformo in stringa
+    if (reverseWord == wordToCheck){
+        return `"${myWord}" è palindroma`;
+    } else {
+        return `"${myWord}" non è palindroma`;
     }
 }
 
-// Printing the answer
-console.log(palindroma(word));
+// Printing the answer if the word is not a number
+if (isNaN(Number(word))){
+    console.log(palindroma(word));
+} else {
+    console.log("Inserisci una stringa, non un numero");
+}
